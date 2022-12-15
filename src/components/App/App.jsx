d09f10ce92b42a2febd9824efef0e4ba658197f9
olhazamlynska <olya.sonechko@gmail.com>
2022-12-15 17:15:58 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,15 +9,34 @@ import { FilterContacts } from 'components/FilterContacts/FilterContacts';
 import { Box } from 'components/Box/Box';
 
 export class App extends Component {
-  state = {
-    contacts: [
+  static defaultProps = {
+    intitialContacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+  };
+
+  state = {
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (savedContacts !== null) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    } else {
+      this.setState({ contacts: this.props.intitialContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (name, number) => {
     const { contacts } = this.state;
